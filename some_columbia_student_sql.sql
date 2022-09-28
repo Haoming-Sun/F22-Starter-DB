8bd2bf2a024cebbba125c46fea6bb1b0dc1ff8a6
Haoming_Sun <haoming_sun_2019@163.com>
2022-09-28 16:03:58 -0400

    1

---
+++ some_columbia_student_sql.sql
@@ -0,0 +1,33 @@
+/*
+ Create table statement.
+ */
+use f22_databases;
+create table columbia_student
+(
+    auto_id     int auto_increment
+        primary key,
+    last_name   varchar(128) not null,
+    first_name  varchar(128) not null,
+    middle_name varchar(128) null,
+    email       varchar(256) not null,
+    uni         varchar(12)  not null,
+    constraint columbia_student_email_uindex
+        unique (email),
+    constraint columbia_student_uni_uindex
+        unique (uni)
+);
+
+/*
+ Example insert statement. Note that since auto_id is auto-increment, the insert
+ statement does not need to specify it.
+ */
+ insert into columbia_student (last_name, first_name, middle_name, email, uni)
+    values ("Ferguson", "Donald", "Francis", "dff@cs.columbia.edu", "dff9");
+
+ insert into columbia_student (last_name, first_name, middle_name, email, uni)
+    values ("Ferguson", "Donald", "Francis", "dff@cs.columbia.edu", "dff9");
+SELECT * FROM columbia_student;
+
+insert into f22_databases.columbia_students(guid, last_name, first_name, middle_name, email, school_code)
+    values('hs3220','Sun','Haoming','','hs3220@columbia.edu','SEAS')
+
